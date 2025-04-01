2d2ca5ced23913bb4496b580c2f72eb3ae0f850b
Meiji <83084978+MeijiShotan@users.noreply.github.com>
2025-04-01 10:31:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if uploaded_file is not None:
     st.image(image, caption="รูปที่อัปโหลด", use_column_width=True)
 
     # ลดขนาดภาพเพื่อให้ API รองรับ
-    max_size = (512, 512)
+    max_size = (640, 640)
     image.thumbnail(max_size)
 
     with st.spinner("กำลังประมวลผล..."):
